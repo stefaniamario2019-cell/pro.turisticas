c0209641fa9148eea6e6e00ab26d204c022749b1
stefaniamario2019-cell <stefaniamario2019@gmail.com>
2026-04-17 15:44:39 +0200

    Update PropertiesGrid.js

--- components/PropertiesGrid.js
+++ components/PropertiesGrid.js
@@ -1,10 +1,10 @@
 import PropertyCard from './PropertyCard'
 
 export default function PropertiesGrid({ properties }) {
-  if (!properties) return <p>Cargando...</p>
+  if (!properties) return <p className="p-6">Cargando...</p>
 
   return (
-    <div className="p-6 grid md:grid-cols-3 gap-6">
+    <div className="max-w-7xl mx-auto p-6 grid md:grid-cols-3 gap-8">
       {properties.map((p) => (
         <PropertyCard key={p.id} property={p} />
       ))}
